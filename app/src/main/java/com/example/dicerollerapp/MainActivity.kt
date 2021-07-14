6e4c7e0d9828c8e0a104bc170f6b8f4df8655be2
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-13 17:32:35 -0700

    added the text, will be shown after the utton is clicked

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dicerollerapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,7 +12,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //CREATING THIS TO MAKE THE APP, INTERACTIVE
         val rollButton : Button = findViewById(R.id.button)
+        // We create this so that it can be seen after button click
         rollButton.setOnClickListener {
+            val resultTextView: TextView=findViewById(R.id.textView)
+            resultTextView.text="6"
          val toast=   Toast.makeText(this,"DICE ROLLED SUCCESSFULLY!",Toast.LENGTH_LONG)
             toast.show()
         }
