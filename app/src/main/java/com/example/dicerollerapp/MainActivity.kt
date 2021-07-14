e4b98c78a5afa140d06c71e921fe928a8624356f
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-13 17:04:30 -0700

    CREATED THE OBJECT OF THE BUTTON, WITH THE HELP OF findViewByid method .

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.dicerollerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //CREATING THIS TO MAKE THE APP, INTERACTIVE
+        val rollButton : Button = findViewById(R.id.button)
+        
     }
 }
